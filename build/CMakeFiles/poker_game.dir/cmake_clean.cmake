77990e6052fb5c9f00c9f1b48ca14b468c0e28c7
Siddharth Raina <siddharth.raina@Oculus.com>
2017-05-10 02:45:21 -0400

    Initial Commit

---
+++ build/CMakeFiles/poker_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poker_game.dir/src/poker.cpp.o"
+  "poker_game.pdb"
+  "poker_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poker_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
